9b508ce47117602d69bfe3a3cf38e32115615034
Ziru Zou <jiyucoding@yahoo.com>
2023-03-20 22:15:46 -0400

    1-m relationship, show animal, zookeeper, enclosure by id with info about the related tables

--- server/app.py
+++ server/app.py
@@ -19,16 +19,52 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ""
+    animal = Animal.query.filter_by(id=id).first()
+    zookeeper = animal.zookeeper
+    animal_dict = {
+        "name": animal.name,
+        "species": animal.species,
+        "zookeeper": {
+            "name": zookeeper.name,
+            "birthday": zookeeper.birthday
+        }
+    }
+    return make_response(animal_dict, 200)
 
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ""
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+    animals_dict = []
+    for animal in zookeeper.animals:
+        animal_dict = {
+            "name": animal.name,
+            "species": animal.species
+        }
+        animals_dict.append(animal_dict)
+    zookeeper_dict = {
+        "name": zookeeper.name,
+        "birthday": zookeeper.birthday,
+        "animals": animals_dict
+    }
+    return make_response(zookeeper_dict, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ""
+    enclosure = Enclosure.query.filter_by(id=id).first()
+    animals_dict = []
+    for animal in enclosure.animals:
+        animal_dict = {
+            "name": animal.name,
+            "species": animal.species
+        }
+        animals_dict.append(animal_dict)
+    enclosure_dict = {
+        "environment": enclosure.environment,
+        "open_to_visitors": enclosure.open_to_visitors,
+        "animals": animals_dict
+    }
+    return make_response(enclosure_dict, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,15 +7,35 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.DateTime)
+    animals = db.relationship('Animal', backref="zookeeper")    # animal.zookeeper
+
+    def __repr__(self):
+        return f'''<Zookeeper ID: {self.id}\nName: {self.name}\nBirthday: {self.birthday}\n'''
 
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref="enclosure")
+
+    def __repr__(self):
+        return f'''<Enclosure ID: {self.id}\Environment: {self.environment}\nOpen_to_visitors: {self.open_to_visitors}\n'''
    
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
+
+    def __repr__(self):
+        return f'''<Animal ID: {self.id}\nName: {self.name}\nspecies: {self.species}\n'''
